3ddfb0a53692d4602b9559644145e37796cca1b7
s5r <sakher.x@gmail.com>
2024-07-14 22:37:02 -0400

    run 4 evaluate

--- run.sh
+++ run.sh
@@ -30,22 +30,18 @@ nohup python -u train.py \
   --attention_dropout 0.0
 elif [ $1 == "test" ]
 then
-nohup python -u train.py \
+nohup python -u evaluate.py \
+  --checkpoint_path $2 \
   --dataset_directory $3 \
-  --data_size 1 \
+  --repo_token $4 \
+  --data_size 1.0 \
   --model_id meta-llama/Llama-2-7b-hf \
   --entities_filename entity2text.txt \
   --descriptions_filename entity2textlong.txt \
   --padding 50 \
   --batch_size 32 \
-  --patience 3 \
-  --repo_token $4 \
-  --checkpoint_path $2 \
-  --learning_rate  5e-5 \
-  --decay 0.25 \
-  --epochs 5 \
   --verbose True \
-  --attention_dropout 0.0 \
+  --attention_dropout 0.0
 else
   echo 'Please specify test or train.'
 fi
